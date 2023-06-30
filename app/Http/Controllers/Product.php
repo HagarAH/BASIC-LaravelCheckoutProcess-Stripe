4b0e47df576020b2fbebbed5528d5b24e477e02e
Hagar Hamad <hagar.akrum@gmail.com>
2023-06-30 16:39:31 +0300

    backend: webhook added

--- app/Http/Controllers/Product.php
+++ app/Http/Controllers/Product.php
@@ -59,10 +59,6 @@ class Product extends Controller
         return redirect($checkout_session->url);
     }
 
-    public function cancel()
-    {
-
-    }
 
     /**
      * @throws ApiErrorException
@@ -86,16 +82,59 @@ class Product extends Controller
         throw new NotFoundHttpException();
         }
 
-        $order=Order::where('session_id',$session->id)->where('status','unpaid')->first();
-        if(!$order){
-            throw new NotFoundHttpException();
+
+        $order=Order::where('session_id',$sessionId)->first();
+        if($order && $order->status==='unpaid'){
+            $order->status='paid';
+            $order->save();
         }
-        $order->status='paid';
-        $order->save();
+
 
         return view('product.checkout.success', compact('customer'));
     }
     public function webhook(){
+        $stripe = new StripeClient(env('STRIPE_SECRETKEY'));
+
+        $endpoint_secret = env('stripe_webhook_secret');
+
+        $payload = @file_get_contents('php://input');
+        $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
+        $event = null;
+
+        try {
+            $event = \Stripe\Webhook::constructEvent(
+                $payload, $sig_header, $endpoint_secret
+            );
+        } catch(\UnexpectedValueException $e) {
+            // Invalid payload
+            http_response_code(400);
+
+            exit();
+        } catch(\Stripe\Exception\SignatureVerificationException $e) {
+            // Invalid signature
+            http_response_code(400);
+            exit();
+        }
+
+// Handle the event
+        switch ($event->type) {
+            case 'payment_intent.succeeded':
+                $session = $event->data->object;
+                $sessionId=$session->id;
+
+                $order=Order::where('session_id',$sessionId)->first();
+                if($order && $order->status==='unpaid'){
+                    $order->status='paid';
+                    $order->save();
+                }
+
+
+            // ... handle other event types
+            default:
+                echo 'Received unknown event type ' . $event->type;
+        }
+
+        http_response_code(200);
 
     }
 
